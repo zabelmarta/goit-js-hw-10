abd70580d867e362702390545bc29ce1111266a0
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-02-04 01:36:39 +0100

    fetch

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+const baseUrl = 'https://restcountries.com/v3.1/name';
+
+const fetchCountries = name => {
+  return fetch(`${baseUrl}/${name}?fields=name,capital,population,flags,languages`).then(
+    response => {
+      if (response.status === 404) {
+        return Promise.reject(new Error());
+      }
+      return response.json();
+    },
+  );
+};
+
+export { fetchCountries };
